9f34bee9e6965379e6406078042aa11e7835bd3c
ashclegit <aashwin@clemson.edu>
2018-04-04 22:07:13 -0400

    Add files via upload

---
+++ copydata.sql
@@ -0,0 +1,47 @@
+copy call_center from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/call_center.csv' with csv  delimiter '|';
+
+copy customer from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/customer.csv' with csv  delimiter '|';
+
+copy catalog_page from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/catalog_page.csv' with csv  delimiter '|';
+
+copy catalog_returns from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/catalog_returns.csv' with csv  delimiter '|';
+
+copy catalog_sales from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/catalog_sales.csv' with csv  delimiter '|';
+
+copy customer_address from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/customer_address.csv' with csv  delimiter '|';
+
+copy customer_demographics from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/customer_demographics.csv' with csv  delimiter '|';
+
+copy date_dim from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/date_dim.csv' with csv  delimiter '|';
+
+copy household_demographics from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/household_demographics.csv' with csv  delimiter '|';
+
+copy income_band from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/income_band.csv' with csv  delimiter '|';
+
+copy inventory from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/inventory.csv' with csv  delimiter '|';
+
+copy item from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/item.csv' with csv  delimiter '|';
+
+copy promotion from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/promotion.csv' with csv  delimiter '|';
+
+copy reason from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/reason.csv' with csv  delimiter '|';
+
+copy ship_mode from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/ship_mode.csv' with csv  delimiter '|';
+
+copy store from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/store.csv' with csv  delimiter '|';
+
+copy store_returns from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/store_returns.csv' with csv  delimiter '|';
+
+copy store_sales from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/store_sales.csv' with csv  delimiter '|';
+
+copy time_dim from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/time_dim.csv' with csv  delimiter '|';
+
+copy warehouse from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/warehouse.csv' with csv  delimiter '|';
+
+copy web_page from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/web_page.csv' with csv  delimiter '|';
+
+copy web_returns from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/web_returns.csv' with csv  delimiter '|';
+
+copy web_sales from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/web_sales.csv' with csv  delimiter '|';
+
+copy web_site from '/home/ubuntu/Downloads/v2.7.0/tools/testfolder/processed/web_site.csv' with csv  delimiter '|';
